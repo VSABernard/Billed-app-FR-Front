1daca5966cabb489940cf67e10f78651912f9c94
Veronica <veronica.stevania@gmail.com>
2022-10-06 10:40:27 +0200

    Correction test integration NewBill : extension file jpg-jpeg-png (with and without msg error)

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -36,168 +36,55 @@ describe("Given I am connected as an employee", () => {
 
   // THE ROAD TO THE RIGHT PAGE
   window.onNavigate(ROUTES_PATH.NewBill)
-  })
+  
   afterEach(() => {
     document.body.innerHTML = ""
   })
 
+  //********** TEST EMAIL ICON VERTICAL HIGHLIGHTED
   describe("When I am on NewBill Page", () => {
-
-      //********** TEST EMAIL ICON VERTICAL HIGHLIGHTED
       test('Then email icon in vertical layout should be highlighted', async () => {
         const emailIcon = screen.getByTestId("icon-mail");
         expect(emailIcon.classList.contains("active-icon")).toBe(true)
       })
     })
-
-  describe("When I am on NewBill Page and I upload a file with an extension other than jpg, jpeg or png", () => {
     
-    //*********** TEST FILE UPLOADED EXTENSION OTHER THAN JPG, JPEG, PNG : DISPLAY ERROR MESSAGE
-    test("Then an error message for the file input should be displayed", () => {
+  //*********** TEST FILE UPLOADED EXTENSION OTHER THAN JPG, JPEG, PNG : DISPLAY ERROR MESSAGE
+  describe("When I am on NewBill page and I upload a file with an extension other than jpg, jpeg or png", () => {
+    beforeEach(() => {
       const html = NewBillUI()
       document.body.innerHTML = html
-
-      // PROPOSE THESE BEHAVIORS FOR THE CONSTRUCTION OF THE PAGE
-      const newBill = new NewBill({
-        document,
-        onNavigate,
-        store: null,
-        bills: bills,
-        localStorage: window.localStorage,
+  
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
       })
-
-      // MOCK THE CALLED FUNCTION
-      const handleChangeFileMock = jest.fn((e) => newBill.handleChangeFile(e))
-
-      // RETRIEVE THE INPUT OF THE FILE TO ADD
-      const fileInput = screen.getByTestId('file')
-      fileInput.addEventListener('change', handleChangeFileMock)
-
-      fireEvent.change(fileInput, {
-        target: {
-          files: [
-            new File(['test'], 'test.pdf', {
-              type: 'application/pdf',
-            }),
-          ],
-        },
-      })
-
-      expect(handleChangeFileMock).toHaveBeenCalled()
-
-      expect(fileInput.files[0].name).toBe('test.pdf')
-
-      const errorMessage = screen.getByTestId('error')
-
-      expect(errorMessage).not.toHaveClass('hidden')
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+          email: "jhondoe@mail.com",
+        })
+      )
     })
-
-    //*********** TEST FILE UPLOADED EXTENSION OTHER THAN JPG, JPEG, PNG : NO ERROR MESSAGE
-    test('Then no error message should be displayed', () => {
-      document.body.innerHTML = NewBillUI()
-
-      // PROPOSE THESE BEHAVIORS FOR THE CONSTRUCTION OF THE PAGE
-      const newBill = new NewBill({
-        document,
-        onNavigate,
-        store: mockStore,
-        bills: bills,
-        localStorage: window.localStorage,
-      })
-
-      // MOCK THE CALLED FUNCTION
-      const handleChangeFileMock = jest.fn((e) => newBill.handleChangeFile(e))
-
-      // RETRIEVE THE INPUT OF THE FILE TO ADD
-      const fileInput = screen.getByTestId('file')
-      fileInput.addEventListener('change', handleChangeFileMock)
-
-      fireEvent.change(fileInput, {
-        target: {
-          files: [
-            new File(['test'], 'test.jpg', {
-              type: 'image/jpeg',
-            }),
-          ],
-        },
-      })
-
-      expect(handleChangeFileMock).toHaveBeenCalled()
-
-      expect(fileInput.files[0].name).toBe('test.jpg')
-
-      const errorMessage = screen.getByTestId('error')
-
-      expect(errorMessage.classList.contains('active')).toBeFalsy()
-    })
-})
-
-//************ TEST INTEGRATION POST
-
-describe('Given I am connected as an employéé', () => {
-  beforeEach(() => {
-    const html = NewBillUI()
-    document.body.innerHTML = html
-
-    Object.defineProperty(window, "localStorage", {
-      value: localStorageMock,
-    })
-    window.localStorage.setItem(
-      "user",
-      JSON.stringify({
-        type: "Employee",
-        email: "jhondoe@mail.com",
-      })
-    )
-  })
-
-  //************* TEST A VALID SUBMIT FORM HAVE BEEN ADDED
-  describe('When I am on NewBill page and submit a valid form', () => {    
-    test('Then a new bill should have been added', async() => {
-      const onNavigate = (pathname) => {
-        document.body.innerHTML = ROUTES({ pathname })
-      }
-      const newBill = new NewBill({
-        document,
-        onNavigate,
-        store: mockStore,
-        localStorage: window.localStorage,
-      })
-
-      const inputFile = document.querySelector(`input[data-testid="file"]`)
-      const onFileChange = jest.fn(newBill.handleChangeFile)
-
-      const eventLoadFilePNG = {
-        target: {
-          files: [new File(['...'], 'test.png', { type: 'document/png' })],
-        },
-      }
-
-      inputFile.addEventListener('change', onFileChange)
-      fireEvent.change(inputFile, eventLoadFilePNG)
-
-      expect(inputFile.files.length).toBe(1)
-      expect(onFileChange).toHaveBeenCalled()
-      expect(inputFile.files[0].name).toBe('test.png')
-    })
-  })
-
-  //************* TEST A NON VALID FORM SUBMITED THEN AN ERROR MSG DISPLAYED
-  describe('When I am on NewBill page and submit a non valid form', () => {  
-    test("Then an error message should be displayed", async () => {
+    
+    test("Then an error message for the file input should be displayed", () => {
       const onNavigate = (pathname) => {
         document.body.innerHTML = ROUTES({ pathname })
       }
 
+      // PROPOSE THESE BEHAVIORS FOR THE CONSTRUCTION OF THE PAGE
       const newBill = new NewBill({
-        document,
-        onNavigate,
-        store: mockStore,
-        localStorage: window.localStorage,
+         document,
+         onNavigate,
+         store: null,
+         bills: bills,
+         localStorage: window.localStorage,
       })
 
       const inputFile = document.querySelector(`input[data-testid="file"]`)
       const error = screen.getByTestId('error')
+
+      // MOCK THE CALLED FUNCTION
       const onFileChange = jest.fn((e) => newBill.handleChangeFile(e))
 
       const filePDF = new File(['img'], 'test.pdf', { type: 'text/png' })
@@ -215,6 +102,80 @@ describe('Given I am connected as an employéé', () => {
   })
 
 
+  //*********** TEST FILE UPLOADED EXTENSION ON JPG, JPEG, PNG : NO ERROR MESSAGE
+  describe("When I am on NewBill page and I upload a file with an extension jpg, jpeg or png", () => {
+    beforeEach(() => {
+      const html = NewBillUI()
+      document.body.innerHTML = html
+          Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      })
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+          email: "jhondoe@mail.com",
+        })
+      )
+    })
 
+    test('Then no error message should be displayed', () => {
+      const onNavigate = (pathname) => {
+         document.body.innerHTML = ROUTES({ pathname })
+      }
+
+      document.body.innerHTML = NewBillUI()
+
+      // PROPOSE THESE BEHAVIORS FOR THE CONSTRUCTION OF THE PAGE
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        store: mockStore,
+        bills: bills,
+        localStorage: window.localStorage,
+      })
+
+      // MOCK THE CALLED FUNCTION
+      const handleChangeFileMock = jest.fn((e) => newBill.handleChangeFile(e))
+
+      // RETRIEVE THE INPUT OF THE FILE TO ADD
+      const fileInput = document.querySelector(`input[data-testid="file"]`)
+      fileInput.addEventListener('change', handleChangeFileMock)
+
+      const eventLoadFilePNG = {
+        target: {files: [new File(['...'], 'test.jpg', {type: 'image/jpeg'})],},
+      }
+      fireEvent.change(fileInput, eventLoadFilePNG)
+
+      expect(handleChangeFileMock).toHaveBeenCalled()
+
+      expect(fileInput.files[0].name).toBe('test.jpg')
+
+      const errorMessage = screen.getByTestId('error')
+
+      expect(errorMessage.classList.contains('active')).toBeFalsy()
+    })
+  })
 })
 
+//************ TEST INTEGRATION POST
+
+describe('Given I am connected as an employéé', () => {
+ 
+  //************* TEST A VALID SUBMIT FORM HAVE BEEN ADDED
+  describe('When I am on NewBill page and submit a valid form', () => {    
+    test('Then a new bill should have been added', async() => {
+      
+    })
+  })
+
+  //************* TEST A NON VALID FORM SUBMITED THEN AN ERROR MSG DISPLAYED
+  describe('When I am on NewBill page and submit a non valid form', () => {  
+    test("Then an error message should be displayed", async () => {
+      
+    })
+  })
+
+  
+
+})
